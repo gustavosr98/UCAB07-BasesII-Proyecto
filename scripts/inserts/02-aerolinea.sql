c831598c16aea4398e5fb626ae69b0206a35dc28
Gustavo Sanchez <gustavosr98@gmail.com>
2019-11-21 19:24:58 -0400

    INSERTS / AVIONES - Aerolinea lista

--- scripts/inserts/02-aerolinea.sql
+++ scripts/inserts/02-aerolinea.sql
@@ -1,30 +1,30 @@
 INSERT INTO Aerolinea(nombre,tipo,logo) 
-	VALUES('Aeropostal','REGIONAL',BFILENAME('DIR_IMG_PROYECTO','aeropostal.png'));
+	VALUES('Aeropostal','REGIONAL',BFILENAME('DIR_IMG_PROYECTO','aerolinea/aeropostal.png'));
 INSERT INTO Aerolinea(nombre,tipo,logo) 
-	VALUES('Aerotuy','REGIONAL',BFILENAME('DIR_IMG_PROYECTO','aerotuy.png'));
+	VALUES('Aerotuy','REGIONAL',BFILENAME('DIR_IMG_PROYECTO','aerolinea/aerotuy.png'));
 INSERT INTO Aerolinea(nombre,tipo,logo) 
-	VALUES('Aserca Airlines','REGIONAL',BFILENAME('DIR_IMG_PROYECTO','aserca.jpg'));
+	VALUES('Aserca Airlines','REGIONAL',BFILENAME('DIR_IMG_PROYECTO','aerolinea/aserca.jpeg'));
 INSERT INTO Aerolinea(nombre,tipo,logo) 
-	VALUES('Venezolana','REGIONAL',BFILENAME('DIR_IMG_PROYECTO','venezolana.png'));
+	VALUES('Venezolana','REGIONAL',BFILENAME('DIR_IMG_PROYECTO','aerolinea/venezolana.png'));
 INSERT INTO Aerolinea(nombre,tipo,logo) 
-	VALUES('Albatros','REGIONAL',BFILENAME('DIR_IMG_PROYECTO','albatros.jpg'));
+	VALUES('Albatros','REGIONAL',BFILENAME('DIR_IMG_PROYECTO','aerolinea/albatros.jpeg'));
 INSERT INTO Aerolinea(nombre,tipo,logo) 
-	VALUES('Conviasa','RED',BFILENAME('DIR_IMG_PROYECTO','conviasa.jpg'));
+	VALUES('Conviasa','RED',BFILENAME('DIR_IMG_PROYECTO','aerolinea/conviasa.png'));
 INSERT INTO Aerolinea(nombre,tipo,logo) 
-	VALUES('Laser Airlines','RED',BFILENAME('DIR_IMG_PROYECTO','laser.jpg'));
+	VALUES('Laser Airlines','RED',BFILENAME('DIR_IMG_PROYECTO','aerolinea/laser.png'));
 INSERT INTO Aerolinea(nombre,tipo,logo) 
-	VALUES('Avior Airlines','RED',BFILENAME('DIR_IMG_PROYECTO','avior.jpg'));
+	VALUES('Avior Airlines','RED',BFILENAME('DIR_IMG_PROYECTO','aerolinea/avior.png'));
 INSERT INTO Aerolinea(nombre,tipo,logo) 
-	VALUES('Rutaca Airlines','RED',BFILENAME('DIR_IMG_PROYECTO','rutaca.png'));
+	VALUES('Rutaca Airlines','RED',BFILENAME('DIR_IMG_PROYECTO','aerolinea/rutaca.png'));
 INSERT INTO Aerolinea(nombre,tipo,logo) 
-	VALUES('Estelar','RED',BFILENAME('DIR_IMG_PROYECTO','estelar.jpg'));
+	VALUES('Estelar','RED',BFILENAME('DIR_IMG_PROYECTO','aerolinea/estelar.png'));
 INSERT INTO Aerolinea(nombre,tipo,logo) 
-	VALUES('Copa Airlines','GRAN_ESCALA',BFILENAME('DIR_IMG_PROYECTO','copa.jpg'));
+	VALUES('Copa Airlines','GRAN_ESCALA',BFILENAME('DIR_IMG_PROYECTO','aerolinea/copa.png'));
 INSERT INTO Aerolinea(nombre,tipo,logo) 
-	VALUES('American Airlines','GRAN_ESCALA',BFILENAME('DIR_IMG_PROYECTO','american.jpg'));
+	VALUES('American Airlines','GRAN_ESCALA',BFILENAME('DIR_IMG_PROYECTO','aerolinea/american.jpeg'));
 INSERT INTO Aerolinea(nombre,tipo,logo) 
-	VALUES('Avianca','GRAN_ESCALA',BFILENAME('DIR_IMG_PROYECTO','avianca.jpg'));
+	VALUES('Avianca','GRAN_ESCALA',BFILENAME('DIR_IMG_PROYECTO','aerolinea/avianca.png'));
 INSERT INTO Aerolinea(nombre,tipo,logo) 
-	VALUES('Wingo','GRAN_ESCALA',BFILENAME('DIR_IMG_PROYECTO','wingo.jpg'));
+	VALUES('Wingo','GRAN_ESCALA',BFILENAME('DIR_IMG_PROYECTO','aerolinea/wingo.jpg'));
 INSERT INTO Aerolinea(nombre,tipo,logo) 
-	VALUES('Air Europa','GRAN_ESCALA',BFILENAME('DIR_IMG_PROYECTO','aireuropa.jpg'));
+	VALUES('Air Europa','GRAN_ESCALA',BFILENAME('DIR_IMG_PROYECTO','aerolinea/aireuropa.png'));
