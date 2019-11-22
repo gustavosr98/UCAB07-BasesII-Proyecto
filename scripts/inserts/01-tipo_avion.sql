fe674b99a3dd793ce082459ee9dd6899d46b81c3
Gustavo Sanchez <gustavosr98@gmail.com>
2019-11-22 01:30:53 -0400

    INSERT / AVIONES - Agregar capacidad a aviones

--- scripts/inserts/01-tipo_avion.sql
+++ scripts/inserts/01-tipo_avion.sql
@@ -1,9 +1,10 @@
 INSERT INTO Tipo_Avion (
-	id,nombre,modelo,foto,velocidad_max,
+	id,nombre,modelo,capacidad,foto,velocidad_max,
 	alcance_max,altitud_max,evergadura,
 	ancho_interior_cabina,altura_interior_cabina) 
 VALUES (
 	DEFAULT,'Boeing','717-200',
+	106,
 	BFILENAME('DIR_IMG_PROYECTO','tipo_avion/01-Boeing_717-200.jpg'),
 	UNIDAD('VELOCIDAD','MACH',0.656787),
 	UNIDAD('LONGITUD','KILOMETRO',3815),
@@ -14,11 +15,12 @@ VALUES (
 );
 
 INSERT INTO Tipo_Avion (
-	id,nombre,modelo,foto,velocidad_max,
+	id,nombre,modelo,capacidad,foto,velocidad_max,
 	alcance_max,altitud_max,evergadura,
 	ancho_interior_cabina,altura_interior_cabina) 
 VALUES (
 	DEFAULT,'Airbus A340','-200',
+	303,
 	BFILENAME('DIR_IMG_PROYECTO','tipo_avion/02-Airbus_A340-200.jpg'),
 	UNIDAD('VELOCIDAD','MACH',0.86),
 	UNIDAD('LONGITUD','KILOMETRO',14100),
@@ -29,11 +31,12 @@ VALUES (
 );
 
 INSERT INTO Tipo_Avion (
-	id,nombre,modelo,foto,velocidad_max,
+	id,nombre,modelo,capacidad,foto,velocidad_max,
 	alcance_max,altitud_max,evergadura,
 	ancho_interior_cabina,altura_interior_cabina) 
 VALUES (
 	DEFAULT,'McDonnell Douglas','MD-80',
+	155,
 	BFILENAME('DIR_IMG_PROYECTO','tipo_avion/03-MD-80.jpg'),
 	UNIDAD('VELOCIDAD','MACH',0.8),
 	UNIDAD('LONGITUD','KILOMETRO',3300),
@@ -44,11 +47,12 @@ VALUES (
 );
 
 INSERT INTO Tipo_Avion (
-	id,nombre,modelo,foto,velocidad_max,
+	id,nombre,modelo,capacidad,foto,velocidad_max,
 	alcance_max,altitud_max,evergadura,
 	ancho_interior_cabina,altura_interior_cabina) 
 VALUES (
 	DEFAULT,'Embraer E-Jet','E170',
+	78,
 	BFILENAME('DIR_IMG_PROYECTO','tipo_avion/04-Embraer-E170.jpg'),
 	UNIDAD('VELOCIDAD','MACH',0.82),
 	UNIDAD('LONGITUD','KILOMETRO',3982),
@@ -59,11 +63,12 @@ VALUES (
 );
 
 INSERT INTO Tipo_Avion (
-	id,nombre,modelo,foto,velocidad_max,
+	id,nombre,modelo,capacidad,foto,velocidad_max,
 	alcance_max,altitud_max,evergadura,
 	ancho_interior_cabina,altura_interior_cabina) 
 VALUES (
 	DEFAULT,'Boeing','747-8',
+	467,
 	BFILENAME('DIR_IMG_PROYECTO','tipo_avion/05-Boeing-747-8.jpg'),
 	UNIDAD('VELOCIDAD','MACH',0.86),
 	UNIDAD('LONGITUD','KILOMETRO',5462),
@@ -74,11 +79,12 @@ VALUES (
 );
 
 INSERT INTO Tipo_Avion (
-	id,nombre,modelo,foto,velocidad_max,
+	id,nombre,modelo,capacidad,foto,velocidad_max,
 	alcance_max,altitud_max,evergadura,
 	ancho_interior_cabina,altura_interior_cabina) 
 VALUES (
 	DEFAULT,'Boeing','VC-25A',
+	76,
 	BFILENAME('DIR_IMG_PROYECTO','tipo_avion/06-Boeing-VC-25A.jpg'),
 	UNIDAD('VELOCIDAD','MACH',0.92),
 	UNIDAD('LONGITUD','KILOMETRO',13000),
@@ -89,11 +95,12 @@ VALUES (
 );
 
 INSERT INTO Tipo_Avion (
-	id,nombre,modelo,foto,velocidad_max,
+	id,nombre,modelo,capacidad,foto,velocidad_max,
 	alcance_max,altitud_max,evergadura,
 	ancho_interior_cabina,altura_interior_cabina) 
 VALUES (
 	DEFAULT,'Boeing','747SP',
+	343,
 	BFILENAME('DIR_IMG_PROYECTO','tipo_avion/07-Boeing-747SP.jpg'),
 	UNIDAD('VELOCIDAD','MACH',0.92),
 	UNIDAD('LONGITUD','KILOMETRO',13700),
@@ -104,11 +111,12 @@ VALUES (
 );
 
 INSERT INTO Tipo_Avion (
-	id,nombre,modelo,foto,velocidad_max,
+	id,nombre,modelo,capacidad,foto,velocidad_max,
 	alcance_max,altitud_max,evergadura,
 	ancho_interior_cabina,altura_interior_cabina) 
 VALUES (
 	DEFAULT,'Embraer','Phenom 100',
+	7,
 	BFILENAME('DIR_IMG_PROYECTO','tipo_avion/08-Embraer-Phenom-100.jpg'),
 	UNIDAD('VELOCIDAD','MACH',0.7),
 	UNIDAD('LONGITUD','KILOMETRO',2182),
@@ -119,11 +127,12 @@ VALUES (
 );
 
 INSERT INTO Tipo_Avion (
-	id,nombre,modelo,foto,velocidad_max,
+	id,nombre,modelo,capacidad,foto,velocidad_max,
 	alcance_max,altitud_max,evergadura,
 	ancho_interior_cabina,altura_interior_cabina) 
 VALUES (
 	DEFAULT,'ICON','A5',
+	2,
 	BFILENAME('DIR_IMG_PROYECTO','tipo_avion/09-ICON-A5.jpg'),
 	UNIDAD('VELOCIDAD','MACH',0.142533),
 	UNIDAD('LONGITUD','KILOMETRO',791),
@@ -134,11 +143,12 @@ VALUES (
 );
 
 INSERT INTO Tipo_Avion (
-	id,nombre,modelo,foto,velocidad_max,
+	id,nombre,modelo,capacidad,foto,velocidad_max,
 	alcance_max,altitud_max,evergadura,
 	ancho_interior_cabina,altura_interior_cabina) 
 VALUES (
 	DEFAULT,'Bombardier','CRJ200',
+	50,
 	BFILENAME('DIR_IMG_PROYECTO','tipo_avion/10-Bombardier-CRJ200.jpg'),
 	UNIDAD('VELOCIDAD','MACH',0.81),
 	UNIDAD('LONGITUD','KILOMETRO',3056),
@@ -149,11 +159,12 @@ VALUES (
 );
 
 INSERT INTO Tipo_Avion (
-	id,nombre,modelo,foto,velocidad_max,
+	id,nombre,modelo,capacidad,foto,velocidad_max,
 	alcance_max,altitud_max,evergadura,
 	ancho_interior_cabina,altura_interior_cabina) 
 VALUES (
-	DEFAULT,'Ilyushin','Il-96',
+	DEFAULT,'Ilyushin','Il-96M',
+	307,
 	BFILENAME('DIR_IMG_PROYECTO','tipo_avion/11-Ilyushin-Il96.jpg'),
 	UNIDAD('VELOCIDAD','MACH',0.84),
 	UNIDAD('LONGITUD','KILOMETRO',13500),
@@ -164,11 +175,12 @@ VALUES (
 );
 
 INSERT INTO Tipo_Avion (
-	id,nombre,modelo,foto,velocidad_max,
+	id,nombre,modelo,capacidad,foto,velocidad_max,
 	alcance_max,altitud_max,evergadura,
 	ancho_interior_cabina,altura_interior_cabina) 
 VALUES (
 	DEFAULT,'Tupolev','Tu-204',
+	210,
 	BFILENAME('DIR_IMG_PROYECTO','tipo_avion/12-Tupolev-Tu-204.jpg'),
 	UNIDAD('VELOCIDAD','MACH',0.728863),
 	UNIDAD('LONGITUD','KILOMETRO',4300),
@@ -179,11 +191,12 @@ VALUES (
 );
 
 INSERT INTO Tipo_Avion (
-	id,nombre,modelo,foto,velocidad_max,
+	id,nombre,modelo,capacidad,foto,velocidad_max,
 	alcance_max,altitud_max,evergadura,
 	ancho_interior_cabina,altura_interior_cabina) 
 VALUES (
 	DEFAULT,'Tupolev','Tu-214',
+	210,
 	BFILENAME('DIR_IMG_PROYECTO','tipo_avion/13-Tupolev-Tu-214.jpg'),
 	UNIDAD('VELOCIDAD','MACH',0.733),
 	UNIDAD('LONGITUD','KILOMETRO',4340),
@@ -194,11 +207,12 @@ VALUES (
 );
 
 INSERT INTO Tipo_Avion (
-	id,nombre,modelo,foto,velocidad_max,
+	id,nombre,modelo,capacidad,foto,velocidad_max,
 	alcance_max,altitud_max,evergadura,
 	ancho_interior_cabina,altura_interior_cabina) 
 VALUES (
 	DEFAULT,'Tupolev','Tu-204SM',
+	194,
 	BFILENAME('DIR_IMG_PROYECTO','tipo_avion/14-Tupolev-Tu-204SM.jpg'),
 	UNIDAD('VELOCIDAD','MACH',0.743),
 	UNIDAD('LONGITUD','KILOMETRO',5800),
@@ -209,11 +223,12 @@ VALUES (
 );
 
 INSERT INTO Tipo_Avion (
-	id,nombre,modelo,foto,velocidad_max,
+	id,nombre,modelo,capacidad,foto,velocidad_max,
 	alcance_max,altitud_max,evergadura,
 	ancho_interior_cabina,altura_interior_cabina) 
 VALUES (
 	DEFAULT,'An-225 Mriya','UNICO',
+	600,
 	BFILENAME('DIR_IMG_PROYECTO','tipo_avion/15-An-225-Mriya.jpg'),
 	UNIDAD('VELOCIDAD','MACH',0.688371),
 	UNIDAD('LONGITUD','KILOMETRO',4000),
@@ -223,7 +238,7 @@ VALUES (
 	UNIDAD('LONGITUD','METRO',2.12)
 );
 
-SELECT TA.id,TA.nombre,TA.modelo,TA.foto,TA.velocidad_max.cantidad,TA.
+SELECT TA.id,TA.nombre,TA.modelo,TA,capacidad.foto,TA.velocidad_max.cantidad,TA.
 	alcance_max.cantidad,TA.altitud_max.cantidad,TA.evergadura.cantidad,TA.
 	ancho_interior_cabina.cantidad,TA.altura_interior_cabina.cantidad 
 FROM TIPO_AVION TA;
