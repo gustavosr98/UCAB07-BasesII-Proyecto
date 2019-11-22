98820a534de08495fddc8c53189f95f40a169cc1
Javier Andrade <javierandrade2107@gmail.com>
2019-11-22 03:10:07 -0400

    inserts alojamiento avance

---
+++ scripts/inserts/11-alojamiento.sql
@@ -0,0 +1,99 @@
+INSERT INTO Alojamiento (
+	id,nombre,tipo,foto,fecha_fundacion) 
+VALUES (
+	DEFAULT,
+    'Hilton Hotels & Resorts'
+    'HOTEL',
+	BFILENAME('DIR_IMG_PROYECTO','alojamiento/01-Hilton_Hotels_&_Resorts.jpg'),
+    TO_DATE('31/05/1919', 'dd/mm/yyyy')
+);
+
+INSERT INTO Alojamiento (
+	id,nombre,tipo,foto,fecha_fundacion) 
+VALUES (
+	DEFAULT,
+    'Eurobuilding'
+    'HOTEL',
+	BFILENAME('DIR_IMG_PROYECTO','alojamiento/02-Eurobuilding.jpg'),
+    TO_DATE('10/04/1990', 'dd/mm/yyyy')
+);
+
+INSERT INTO Alojamiento (
+	id,nombre,tipo,foto,fecha_fundacion) 
+VALUES (
+	DEFAULT,
+    'InterContinental Hotels Group'
+    'HOTEL',
+	BFILENAME('DIR_IMG_PROYECTO','alojamiento/03-InterContinental_Hotels_Group.jpg'),
+    TO_DATE('15/04/2003', 'dd/mm/yyyy')
+);
+
+INSERT INTO Alojamiento (
+	id,nombre,tipo,foto,fecha_fundacion) 
+VALUES (
+	DEFAULT,
+    'AccorHotels'
+    'HOTEL',
+	BFILENAME('DIR_IMG_PROYECTO','alojamiento/04-AccorHotels.jpg'),
+    TO_DATE('21/05/1967', 'dd/mm/yyyy')
+);
+
+INSERT INTO Alojamiento (
+	id,nombre,tipo,foto,fecha_fundacion) 
+VALUES (
+	DEFAULT,
+    'Meliá Hotels International'
+    'HOTEL',
+	BFILENAME('DIR_IMG_PROYECTO','alojamiento/05-Melia_Hotels_International.jpg'),
+    TO_DATE('03/08/1956', 'dd/mm/yyyy')
+);
+
+INSERT INTO Alojamiento (
+	id,nombre,tipo,foto,fecha_fundacion) 
+VALUES (
+	DEFAULT,
+    'TUI Hotels & Resorts'
+    'HOTEL',
+	BFILENAME('DIR_IMG_PROYECTO','alojamiento/06-TUI_Hotels_Resorts.png'),
+    TO_DATE('09/10/1923', 'dd/mm/yyyy')
+);
+
+INSERT INTO Alojamiento (
+	id,nombre,tipo,foto,fecha_fundacion) 
+VALUES (
+	DEFAULT,
+    'Best Western International'
+    'HOTEL',
+	BFILENAME('DIR_IMG_PROYECTO','alojamiento/07-Best_Western_International.jpg'),
+    TO_DATE('15/07/1946', 'dd/mm/yyyy')
+);
+
+INSERT INTO Alojamiento (
+	id,nombre,tipo,foto,fecha_fundacion) 
+VALUES (
+	DEFAULT,
+    'Marriott International'
+    'HOTEL',
+	BFILENAME('DIR_IMG_PROYECTO','alojamiento/08-Marriott_International.jpg'),
+    TO_DATE('20/05/1927', 'dd/mm/yyyy')
+);
+
+INSERT INTO Alojamiento (
+	id,nombre,tipo,foto,fecha_fundacion) 
+VALUES (
+	DEFAULT,
+    'Barceló Hotels & Resorts'
+    'HOTEL',
+	BFILENAME('DIR_IMG_PROYECTO','alojamiento/09-Barcelo_Hotels_Resorts.jpg'),
+    TO_DATE('10/07/1931', 'dd/mm/yyyy')
+);
+
+INSERT INTO Alojamiento (
+	id,nombre,tipo,foto,fecha_fundacion) 
+VALUES (
+	DEFAULT,
+    'Iberostar Hotels & Resorts'
+    'HOTEL',
+	BFILENAME('DIR_IMG_PROYECTO','alojamiento/10-Iberostar_Hotels_Resorts.jpg'),
+    TO_DATE('23/02/1956', 'dd/mm/yyyy')
+);
