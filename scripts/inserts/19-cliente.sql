b83feb07043c90c7f13c313f5b2b0ec910ddcf6a
Javier Andrade <javierandrade2107@gmail.com>
2019-11-22 23:32:00 -0400

    inserts cliente

---
+++ scripts/inserts/19-cliente.sql
@@ -0,0 +1,187 @@
+INSERT INTO Cliente (
+	id,primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,sexo,telefono,fecha_nacimiento) 
+VALUES (
+	DEFAULT,
+    'Juan',
+    'Alejandro',
+    'Rodriguez',
+    'Hernandez',
+    'M',
+    '04142002345',    
+    TO_DATE('20/06/1998', 'dd/mm/yyyy')
+);
+
+INSERT INTO Cliente (
+	id,primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,sexo,telefono,fecha_nacimiento) 
+VALUES (
+	DEFAULT,
+    'Jose',
+    'Angel',
+    'Gomez',
+    'Gomez',
+    'M',
+    '04164329956',    
+    TO_DATE('17/03/1960', 'dd/mm/yyyy')
+);
+
+INSERT INTO Cliente (
+	id,primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,sexo,telefono,fecha_nacimiento) 
+VALUES (
+	DEFAULT,
+    'Maria',
+    'Luisa',
+    'Lopez',
+    'Diaz',
+    'F',
+    '04123789666',    
+    TO_DATE('02/02/2008', 'dd/mm/yyyy')
+);
+
+INSERT INTO Cliente (
+	id,primer_nombre,primer_apellido,sexo,telefono,fecha_nacimiento) 
+VALUES (
+	DEFAULT,
+    'Ana',
+    'García',
+    'F',
+    '04126678998',    
+    TO_DATE('13/04/1987', 'dd/mm/yyyy')
+);
+
+INSERT INTO Cliente (
+	id,primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,sexo,telefono,fecha_nacimiento) 
+VALUES (
+	DEFAULT,
+    'Sebastián',
+    'Jorge',
+    'Lopez',
+    'González',
+    'M',
+    '04163445322',    
+    TO_DATE('22/12/2000', 'dd/mm/yyyy')
+);
+
+INSERT INTO Cliente (
+	id,primer_nombre,primer_apellido,segundo_apellido,sexo,telefono,fecha_nacimiento) 
+VALUES (
+	DEFAULT,
+    'Gabriela',
+    'Sosa',
+    'Romero',
+    'F',
+    '04122998345',    
+    TO_DATE('17/04/1991', 'dd/mm/yyyy')
+);
+
+INSERT INTO Cliente (
+	id,primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,sexo,telefono,fecha_nacimiento) 
+VALUES (
+	DEFAULT,
+    'Fernando',
+    'Luis',
+    'Sánchez',
+    'Diaz',
+    'M',
+    '04247778354',    
+    TO_DATE('01/11/1953', 'dd/mm/yyyy')
+);
+
+INSERT INTO Cliente (
+	id,primer_nombre,segundo_nombre,primer_apellido,sexo,telefono,fecha_nacimiento) 
+VALUES (
+	DEFAULT,
+    'Pedro',
+    'Alberto',
+    'Miranda',
+    'M',
+    '04145778624',    
+    TO_DATE('02/04/1984', 'dd/mm/yyyy')
+);
+
+INSERT INTO Cliente (
+	id,primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,sexo,telefono,fecha_nacimiento) 
+VALUES (
+	DEFAULT,
+    'Sara',
+    'Rosa',
+    'Lopez',
+    'Ruiz',
+    'F',
+    '04122267943',    
+    TO_DATE('25/10/1999', 'dd/mm/yyyy')
+);
+
+INSERT INTO Cliente (
+	id,primer_nombre,primer_apellido,sexo,telefono,fecha_nacimiento) 
+VALUES (
+	DEFAULT,
+    'Luis',
+    'Ramirez',
+    'M',
+    '04147764545',    
+    TO_DATE('30/01/2001', 'dd/mm/yyyy')
+);
+
+INSERT INTO Cliente (
+	id,primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,sexo,telefono,fecha_nacimiento) 
+VALUES (
+	DEFAULT,
+    'Silvia',
+    'Fernanda',
+    'Martínez',
+    'Blanco',
+    'F',
+    '04124678331',    
+    TO_DATE('22/05/1973', 'dd/mm/yyyy')
+);
+
+INSERT INTO Cliente (
+	id,primer_nombre,primer_apellido,segundo_apellido,sexo,telefono,fecha_nacimiento) 
+VALUES (
+	DEFAULT,
+    'Rosa',
+    'Pérez',
+    'Torres',
+    'F',
+    '04124347659',    
+    TO_DATE('09/09/1998', 'dd/mm/yyyy')
+);
+
+INSERT INTO Cliente (
+	id,primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,sexo,telefono,fecha_nacimiento) 
+VALUES (
+	DEFAULT,
+    'Gerardo',
+    'Sergio',
+    'Suarez',
+    'Gutiérrez',
+    'M',
+    '04123789666',    
+    TO_DATE('07/06/1963', 'dd/mm/yyyy')
+);
+
+INSERT INTO Cliente (
+	id,primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,sexo,telefono,fecha_nacimiento) 
+VALUES (
+	DEFAULT,
+    'David',
+    'Gabriel',
+    'Fernández',
+    'Lopez',
+    'M',
+    '04148921564',    
+    TO_DATE('11/08/2009', 'dd/mm/yyyy')
+);
+
+INSERT INTO Cliente (
+	id,primer_nombre,segundo_nombre,primer_apellido,segundo_apellido,sexo,telefono,fecha_nacimiento) 
+VALUES (
+	DEFAULT,
+    'Raquel',
+    'Maria',
+    'Muñoz',
+    'Ortiz',
+    'F',
+    '04125566782',    
+    TO_DATE('11/08/1993', 'dd/mm/yyyy')
+);
