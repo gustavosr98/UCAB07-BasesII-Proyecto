dc5e82bc4480365871f031e6f2f27aaef8e7921a
Javier Andrade <javierandrade2107@gmail.com>
2019-12-02 20:37:20 -0400

    sql

--- scripts/inserts/00.sql
+++ scripts/inserts/00.sql
@@ -2,3 +2,9 @@ CREATE DIRECTORY DIR_IMG_PROYECTO AS 'C:/ProyectoBD';
 
 --Limpiar buffer para logs
 SET serveroutput ON SIZE unlimited;
+
+BEGIN
+    DBMS_OUTPUT.ENABLE(NULL);
+END;
+
+ALTER TABLE RESERVACION ADD v_es_ida CHAR(1) CHECK (v_es_ida = 'T' OR v_es_ida = 'F');
